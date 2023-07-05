92d5ccf9268a24936a336981bba1adb2bc03f965
Paul Tanger <detroitstylz@gmail.com>
2023-07-05 13:16:12 -0600

    clean up script

--- src/button_timer.py
+++ src/button_timer.py
@@ -3,19 +3,7 @@ import time
 import pygame
 import RPi.GPIO as GPIO
 
-print("""buttons.py - Detect which button has been pressed
-
-This example should demonstrate how to:
-1. set up RPi.GPIO to read buttons,
-2. determine which button has been pressed
-
-Press Ctrl+C to exit!
-
-""")
-
 # The buttons on Pirate Audio are connected to pins 5, 6, 16 and 24
-# Boards prior to 23 January 2020 used 5, 6, 16 and 20 
-# try changing 24 to 20 if your Y button doesn't work.
 BUTTONS = [5, 6, 16, 24]
 
 # These correspond to buttons A, B, X and Y respectively
@@ -30,28 +18,68 @@ GPIO.setup(BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # define sound object to play
 pygame.init()
-sound = pygame.mixer.Sound('/home/pi/Music/bowl_1.mp3')
 
+# setup function to customize button time and sounds
+def timer_sound(label, timer, sound):
+    '''
+    inputs:
+    label - the button label
+    timer - the time in minutes for the timer
+    sound - which mp3 sound file to play
+    
+    returns: True
+    '''
+    print(f'button {label} pressed, starting timer for {round(timer/60, 2)} minutes')
+    # TODO show timer on display..
+    time.sleep(timer)
+    print(f'{round(timer/60, 2)} minutes timer finished')
+    # TODO customize this further
+    sound.play()
+        
+    return True
+    
+    
 # "handle_button" will be called every time a button is pressed
-# It receives one argument: the associated input pin.
 def handle_button(pin):
+    '''
+    inputs:
+    the associated input pin
+    '''
     label = LABELS[BUTTONS.index(pin)]
-    print("Button press detected on pin: {} label: {}".format(pin, label))
+    #print(f'Button press detected on pin: {pin} label: {label}')
+    
     if label == 'A':
-        print('button A pressed, starting time')
-        # TODO show timer on display..
-        time.sleep(30)
-        print('30 second timer finished')
-        # TODO play sound here..
-        sound.play()
+        # setup sound
+        sound = pygame.mixer.Sound('/home/pi/Music/bowl_1.mp3')
+        # test with 15 second timer
+        timer_sound(label, 15, sound)
+    if label == 'B':
+        # setup sound
+        sound = pygame.mixer.Sound('/home/pi/Music/bowl_1.mp3')
+        # 10 min timer
+        timer_sound(label, 10*60, sound)
+    if label == 'X':
+        # setup sound
+        sound = pygame.mixer.Sound('/home/pi/Music/bowl_1.mp3')
+        # 16 min timer
+        timer_sound(label, 16*60, sound)
+    if label == 'Y':
+        # setup sound
+        sound = pygame.mixer.Sound('/home/pi/Music/bowl_1.mp3')
+        # 30 min timer
+        timer_sound(label, 30*60, sound)
 
 
-# Loop through out buttons and attach the "handle_button" function to each
-# We're watching the "FALLING" edge (transition from 3.3V to Ground) and
-# picking a generous bouncetime of 100ms to smooth out button presses.
-for pin in BUTTONS:
-    GPIO.add_event_detect(pin, GPIO.FALLING, handle_button, bouncetime=100)
+if __name__ == '__main__':
+    
+    print('starting timer script and waiting for button presses')
+    
+    # Loop through out buttons and attach the "handle_button" function to each
+    # We're watching the "FALLING" edge (transition from 3.3V to Ground) and
+    # picking a generous bouncetime of 100ms to smooth out button presses.
+    for pin in BUTTONS:
+        GPIO.add_event_detect(pin, GPIO.FALLING, handle_button, bouncetime=100)
 
-# Finally, since button handlers don't require a "while True" loop,
-# we pause the script to prevent it exiting immediately.
-signal.pause()
+    # Finally, since button handlers don't require a "while True" loop,
+    # we pause the script to prevent it exiting immediately.
+    signal.pause()
