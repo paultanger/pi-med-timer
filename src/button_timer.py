1f448a0f6ff03d7d808127d2756f2d177a3d4c1c
Paul Tanger <detroitstylz@gmail.com>
2023-07-09 20:40:25 -0600

    add more TODOs

--- src/button_timer.py
+++ src/button_timer.py
@@ -12,6 +12,7 @@ from PIL import ImageFont
 # TODO: improvements: auto shutdown after a couple hours if not used
 # TODO: improvements: don't allow another action if timer already in progress   
 # TODO: customize buttons to program different times etc..
+# TODO: add pause / resume button
 
 # The buttons on Pirate Audio are connected to pins 5, 6, 16 and 24
 BUTTONS = [5, 6, 16, 24]
